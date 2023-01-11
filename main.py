3466ba8f63363ab4d55351b8b3cd4d1d80b96721
grebwerd <grebwerd@gmail.com>
2023-01-11 21:13:58 +0000

    init text to ai_image

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-def main():
-    print("Hello World!")
+import os
+import openai
+import webbrowser
 
-if __name__ == "__main__":
-    main()
+prompt = "A mad unicorn drinking coffee."
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    openai.api_key = ""
+
+    response_summary = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Create an analogy for this phrase: You come most carefully upon your hour.",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(f"response: {response_summary}")
+
+    summary_tokens = response_summary["choices"][0]["text"].split(".")
+
+
+    for summary_token in summary_tokens:
+        print(f"summary_token:  {summary_token}")
+        if summary_token:
+            response = openai.Image.create(
+                prompt=summary_token,
+                n=1,
+                size="1024x1024",
+            )
+
+            response_url = response["data"][0]["url"]
+            webbrowser.register('chrome',
+                                None,
+                                webbrowser.BackgroundBrowser(
+                                    "C://Program Files//Google//Chrome//Application//chrome.exe"))
+            webbrowser.get('chrome').open(response_url)
